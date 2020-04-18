e539c91df2d5cb90d73968ee3404fb9db6ad71d4
Aliaga <navichicken@gmail.com>
2020-04-18 03:05:25 -0500

    primer intento
    problemas con la comunicacion entre js y pytohn

--- main.js
+++ main.js
@@ -39,3 +39,29 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+let pyProc = null
+let pyPort = null
+
+const selectPort = () => {
+  pyPort = 4242
+  return pyPort
+}
+
+const createPyProc = () => {
+  let port = '' + selectPort()
+  let script = path.join(__dirname, 'pycalc', 'api.py')
+  pyProc = require('child_process').spawn('python', [script, port])
+  if (pyProc != null) {
+    console.log('child process success')
+  }
+}
+
+const exitPyProc = () => {
+  pyProc.kill()
+  pyProc = null
+  pyPort = null
+}
+
+app.on('ready', createPyProc)
+app.on('will-quit', exitPyProc)
--- renderer.js
+++ renderer.js
@@ -1,6 +1,27 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const zerorpc = require("./node_modules/zerorpc/lib")
+let client = new zerorpc.Client()
+
+client.connect("tcp://127.0.0.1:4242")
+
+client.invoke("echo", "server ready", (error, res) => {
+  if(error || res !== 'server ready') {
+    console.error(error)
+  } else {
+    console.log("server is ready")
+  }
+})
+
+// let formula = document.querySelector('#formula')
+// let result = document.querySelector('#result')
+
+// formula.addEventListener('input', () => {
+//   client.invoke("calc", formula.value, (error, res) => {
+//     if(error) {
+//       console.error(error)
+//     } else {
+//       result.textContent = res
+//     }
+//   })
+// })
+
+// formula.dispatchEvent(new Event('input'))
